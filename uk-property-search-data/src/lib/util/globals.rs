8af5bb70878adc34f8ab788024e4c29210227b4e
Karl Chan <karl-chan@bitbucket.org>
2022-06-05 09:55:25 +0100

    Make log level configurable

--- uk-property-search-data/src/lib/util/globals.rs
+++ uk-property-search-data/src/lib/util/globals.rs
@@ -1,6 +1,6 @@
 use super::{db::Db, properties::Properties};
 use log::Level;
-use std::sync::Once;
+use std::{str::FromStr, sync::Once};
 
 static INIT: Once = Once::new();
 
@@ -11,11 +11,18 @@ pub struct Globals {
 
 impl Globals {
     pub async fn new() -> Globals {
-        INIT.call_once(|| {
-            simple_logger::init_with_level(Level::Debug).unwrap();
-        });
         let properties = Properties::new();
         let db = Db::new(&properties).await;
+
+        INIT.call_once(|| set_logger(&properties));
+
         Globals { db, properties }
     }
+}
+
+fn set_logger(properties: &Properties) {
+    let level_str = properties.get_string("log.level");
+    let level = Level::from_str(&level_str)
+        .expect(format!("[{:?}] is not a valid log level!", &level_str).as_str());
+    simple_logger::init_with_level(level).unwrap();
 }
