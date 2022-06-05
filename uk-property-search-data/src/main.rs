4d8de9e6add1552d322eefee3f6754b47cd6037f
Karl Chan <karl-chan@bitbucket.org>
2022-06-05 21:31:07 +0100

    Add stopwatch logging

--- uk-property-search-data/src/main.rs
+++ uk-property-search-data/src/main.rs
@@ -6,6 +6,8 @@ use anyhow::Result;
 use clap::Parser;
 use cli::{Cli, CliTask};
 use lib::util::globals::Globals;
+use log::info;
+use stopwatch::Stopwatch;
 use tasks::{
     update_property::update_property, update_schools::update_schools, update_tube::update_tube,
 };
@@ -16,11 +18,13 @@ async fn main() -> Result<()> {
 
     let args = Cli::parse();
     for task in args.task {
+        let sw = Stopwatch::start_new();
         match task {
             CliTask::UpdateProperty => update_property(&globals).await?,
             CliTask::UpdateSchools => update_schools(&globals).await?,
             CliTask::UpdateTube => update_tube(&globals).await?,
         }
+        info!("Completed task [{:?}] in [{:?}].", task, sw.elapsed());
     }
     Ok(())
 }
