b45cf7544c96cd843d1d4db621418398d3da1ab9
Karl Chan <karl-chan@bitbucket.org>
2022-06-11 21:42:25 +0100

    Add turnover page

--- uk-property-search-app/src/pages/TurnoverPage.vue
+++ uk-property-search-app/src/pages/TurnoverPage.vue
@@ -6,9 +6,10 @@ q-page(padding)
     .col-1
       q-select(v-model='action' :options='actionOptions', label='Action')
     .col
-      q-range(v-model='priceRange' :min='minPrice' :max='maxPrice' :step='step' label-always markers)
+      q-range(v-model='priceRange' :min='minDuration' :max='maxDuration' label-always
+             :left-label-value='formatDuration(priceRange.min)' :right-label-value='formatDuration(priceRange.max)')
     .col-shrink
-      q-checkbox(v-model='includeBeyondPriceRange' :label='`Include £${maxPrice}+`')
+      q-checkbox(v-model='includeBeyondPriceRange' :label='`Include ${formatDuration(maxDuration)}+`')
 
   .row.q-my-sm
     q-btn(label='Search' color='secondary' icon-right='search' @click='search' :loading='isLoading')
@@ -17,7 +18,7 @@ q-page(padding)
   leaflet-map.map(:markers='markers')
 
   .row.q-my-sm
-    q-table(title="Property prices" :rows='stationProperties' :columns='columns' :filter='tableFilter' :pagination='paginationOptions' row-key='postcode')
+    q-table(title='Property prices' :rows='stationProperties' :columns='columns' :filter='tableFilter' :pagination='paginationOptions' row-key='postcode')
       template(v-slot:top-right)
         q-input(borderless dense debounce='300' v-model='tableFilter' placeholder='Search')
           template(v-slot:append)
@@ -27,7 +28,6 @@ q-page(padding)
 <script lang="ts">
 import LeafletMap from 'components/LeafletMap.vue'
 import L from 'leaflet'
-import { round } from 'lodash'
 import type { ComputedRef, Ref } from 'vue'
 import { computed, defineComponent, ref } from 'vue'
 import { PropertyAction, PropertySummary } from '../models/property'
@@ -42,7 +42,7 @@ interface StationProperty {
 }
 
 export default defineComponent({
-  name: 'PropertyPage',
+  name: 'TurnoverPage',
   components: {
     LeafletMap
   },
@@ -50,23 +50,13 @@ export default defineComponent({
     const tubeStore = useTubeStore()
     const propertyStore = usePropertyStore()
 
-    const minPrice = 0
-    const maxPrice: ComputedRef<number> = computed(() => {
+    const minDuration = 0
+    const maxDuration: ComputedRef<number> = computed(() => {
       switch (action.value.value) {
       case PropertyAction.Buy:
-        return 2_000_000
+        return 720
       case PropertyAction.Rent:
-        return 4_000
-      default:
-        return 0
-      }
-    })
-    const step: ComputedRef<number> = computed(() => {
-      switch (action.value.value) {
-      case PropertyAction.Buy:
-        return 50_000
-      case PropertyAction.Rent:
-        return 100
+        return 84
       default:
         return 0
       }
@@ -90,8 +80,8 @@ export default defineComponent({
     const action: Ref<{label: string, value: PropertyAction}> = ref(actionOptions[0])
     const numBeds: Ref<number> = ref(2)
     const priceRange: Ref<{min: number, max:number}> = ref({
-      min: minPrice,
-      max: maxPrice
+      min: minDuration,
+      max: maxDuration
     })
     const includeBeyondPriceRange: Ref<boolean> = ref(true)
     const showDetailedTooltip: Ref<boolean> = ref(false)
@@ -109,12 +99,12 @@ export default defineComponent({
     const columns = [
       { name: 'station', label: 'Station', field: (row: StationProperty) => row.station.name, sortable: true, align: 'left' },
       { name: 'zone', label: 'Zone', field: (row: StationProperty) => row.station.zone, format: (zones: number[]) => zones.join(','), sortable: true },
-      { name: 'median', label: 'Median', field: (row: StationProperty) => row.property.stats.price.median, format: formatPrice, sortable: true },
-      { name: 'min', label: 'Min', field: (row: StationProperty) => row.property.stats.price.min, format: formatPrice, sortable: true },
-      { name: 'max', label: 'Max', field: (row: StationProperty) => row.property.stats.price.max, format: formatPrice, sortable: true },
-      { name: 'q1', label: 'Q1', field: (row: StationProperty) => row.property.stats.price.q1, format: formatPrice, sortable: true },
-      { name: 'q3', label: 'Q3', field: (row: StationProperty) => row.property.stats.price.q3, format: formatPrice, sortable: true },
-      { name: 'count', label: 'Count', field: (row: StationProperty) => row.property.stats.price.count, sortable: true },
+      { name: 'median', label: 'Median', field: (row: StationProperty) => row.property.stats.days.median, format: formatDuration, sortable: true },
+      { name: 'min', label: 'Min', field: (row: StationProperty) => row.property.stats.days.min, format: formatDuration, sortable: true },
+      { name: 'max', label: 'Max', field: (row: StationProperty) => row.property.stats.days.max, format: formatDuration, sortable: true },
+      { name: 'q1', label: 'Q1', field: (row: StationProperty) => row.property.stats.days.q1, format: formatDuration, sortable: true },
+      { name: 'q3', label: 'Q3', field: (row: StationProperty) => row.property.stats.days.q3, format: formatDuration, sortable: true },
+      { name: 'count', label: 'Count', field: (row: StationProperty) => row.property.stats.days.count, sortable: true },
       {
         name: 'lines',
         label: 'Lines',
@@ -126,12 +116,12 @@ export default defineComponent({
     ]
 
     async function search () {
-      const isValid = (stationProperty: StationProperty) => stationProperty.property.stats.price.count > 0
+      const isValid = (stationProperty: StationProperty) => stationProperty.property.stats.days.count > 0
       const hasAction = (stationProperty: StationProperty) => stationProperty.property.action === action.value.value
       const hasBeds = (stationProperty: StationProperty) => stationProperty.property.numBeds === numBeds.value
       const withinPriceRange = (stationProperty: StationProperty) =>
-        priceRange.value.min <= stationProperty.property.stats.price.median &&
-         (stationProperty.property.stats.price.median <= priceRange.value.max || includeBeyondPriceRange.value)
+        priceRange.value.min <= stationProperty.property.stats.days.median &&
+         (stationProperty.property.stats.days.median <= priceRange.value.max || includeBeyondPriceRange.value)
 
       isLoading.value = true
       stationProperties.value = allStationProperties.value
@@ -150,29 +140,24 @@ export default defineComponent({
       if (showDetailedTooltip.value) {
         return `<b>${station.name}</b> (Zone ${station.zone.toString()})<br>
         <table><tbody>
-        <tr><td>Avg</td><td>${formatPrice(property.stats.price.median)}</td></tr>
-        <tr><td>Range</td><td>${formatPrice(property.stats.price.min)} - ${formatPrice(property.stats.price.max)}</td></tr>
-        <tr><td>IQR</td><td>${formatPrice(property.stats.price.q1)} - ${formatPrice(property.stats.price.q3)}</td></tr>
-        <tr><td>Count</td><td>${property.stats.price.count}</td></tr> 
+        <tr><td>Avg</td><td>${formatDuration(property.stats.days.median)}</td></tr>
+        <tr><td>Range</td><td>${formatDuration(property.stats.days.min)} - ${formatDuration(property.stats.days.max)}</td></tr>
+        <tr><td>IQR</td><td>${formatDuration(property.stats.days.q1)} - ${formatDuration(property.stats.days.q3)}</td></tr>
+        <tr><td>Count</td><td>${property.stats.days.count}</td></tr>
         <tr><td>Lines</td><td><ul>${station.lines.map(l => `<li>${l}</li>`).join('')}</ul></td></tr>
         </tbody></table>`
       } else {
-        return formatPrice(property.stats.price.median)
+        return formatDuration(property.stats.days.median)
       }
     }
 
-    function formatShortPrice (price: number): string {
-      if (price < 10000) {
-        return `£${round(price)}`
-      } else if (price < 1_000_000) {
-        return `£${round(price / 1000)}k`
-      } else {
-        return `£${(price / 1_000_000).toFixed(1)}M`
+    function formatDuration (days: number): string {
+      switch (action.value.value) {
+      case PropertyAction.Buy:
+        return `${(days / 30).toFixed(1)} months`
+      case PropertyAction.Rent:
+        return `${(days / 7).toFixed(1)} weeks`
       }
-    }
-
-    function formatPrice (price: number): string {
-      return `£${round(price)}`
     }
 
     function updateMarkers (): L.Layer[] {
@@ -184,7 +169,7 @@ export default defineComponent({
             { radius: 10 }
           ).bindTooltip(getDetailedTooltipText(stationProperty))
         } else {
-          const width = 60, height = 20
+          const width = 100, height = 20
           return new L.Marker(
             { lat: property.coordinates[1], lng: property.coordinates[0] },
             {
@@ -192,7 +177,7 @@ export default defineComponent({
                 iconUrl: `data:image/svg+xml,
                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${width}" height="${height}">
                   <rect width="100%" height="100%" style="fill:white; stroke:black; stroke-width:1;" />
-                  <text x="${width / 2}" y="${height / 2}" text-anchor="middle" alignment-baseline="central" font-family="sans-serif">${formatShortPrice(property.stats.price.median)}</text>
+                  <text x="${width / 2}" y="${height / 2}" text-anchor="middle" alignment-baseline="central" font-family="sans-serif">${formatDuration(property.stats.days.median)}</text>
                 </svg>`,
                 iconSize: [width, height],
                 iconAnchor: [width / 2, height / 2]
@@ -205,9 +190,8 @@ export default defineComponent({
     }
 
     return {
-      minPrice,
-      maxPrice,
-      step,
+      minDuration,
+      maxDuration,
       actionOptions,
       numBedsOptions,
       paginationOptions,
@@ -224,6 +208,7 @@ export default defineComponent({
       stationProperties,
       markers,
 
+      formatDuration,
       search
     }
   }
